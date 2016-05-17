834de5d8c570a24f7485a6bb2b91d6cec7aad1bf
vineetasharma <vineetasharma18791@gmail.com>
2016-05-17 14:06:14 +0530

    Angularfy the Content App

---
+++ control/content/controllers/content.home.controller.js
@@ -0,0 +1,10 @@
+'use strict';
+
+(function (angular) {
+    angular
+        .module('geoFencePluginContent')
+        .controller('ContentHomeCtrl', ['$scope',
+            function ($scope) {
+                console.log('--------ContentHomeCtrl Controller Loaded-----');
+            }]);
+})(window.angular);
