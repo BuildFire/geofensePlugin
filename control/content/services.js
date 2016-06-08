b9754d7463597215d27034705af0ee75032194b4
Vineeta Sharma <vineetasharma18791@gmail.com>
2016-06-08 14:51:33 +0530

    Added the highAccuracy enable/disable button

--- control/content/services.js
+++ control/content/services.js
@@ -13,6 +13,40 @@
                 this._tagName = tagName;
             }
 
+            DB.prototype.get = function () {
+                var that = this;
+                var deferred = $q.defer();
+                Buildfire.datastore.get(that._tagName, function (err, result) {
+                    if (err && err.code == CODES.NOT_FOUND) {
+                        return deferred.resolve();
+                    }
+                    else if (err) {
+                        return deferred.reject(err);
+                    }
+                    else {
+                        return deferred.resolve(result);
+                    }
+                });
+                return deferred.promise;
+            };
+            DB.prototype.save = function (item) {
+                var that = this;
+                var deferred = $q.defer();
+                if (typeof item == 'undefined') {
+                    return deferred.reject(new Error(MESSAGES.ERROR.DATA_NOT_DEFINED));
+                }
+                Buildfire.datastore.save(item, that._tagName, function (err, result) {
+                    if (err) {
+                        return deferred.reject(err);
+                    }
+                    else if (result) {
+                        return deferred.resolve(result);
+                    } else {
+                        return deferred.reject(new Error(MESSAGES.ERROR.NOT_FOUND));
+                    }
+                });
+                return deferred.promise;
+            };
             DB.prototype.insert = function (items) {
                 var that = this;
                 var deferred = $q.defer();
